7dc1683149474a6649a5dba87e5c2805d307335e
Arnab Bhattacharjee <clonerx008@gmail.com>
2026-09-05 16:16:55 +0530

    Delete backend directory

--- backend/userconfiguration.py
+++
@@ -1,7 +0,0 @@
-import os
-
-gmail_user = os.environ.get("GMAIL_USER", "your email address")
-gmail_password = os.environ.get("GMAIL_PASSWORD", "your email password")
-JARVIS_API_KEY = os.environ.get("JARVIS_API_KEY", "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")
-
-
